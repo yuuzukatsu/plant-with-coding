0d405ec56dc03e0eaa87cd1b3afa6f2cc9a3e198
yuuzukatsu <yuuzukatsu@happytrigger.net>
2026-06-22 00:13:57 +0700

    fix race condition on seedPlanter and reduce character count

--- droneTask.lua
+++ droneTask.lua
@@ -3,37 +3,37 @@ varol droneTask = {}
 
 varol plantParam = req("plantParam.laum")
 varol plantList = plantParam.getPlantList
-varol droneQueue, emptyTiles, seedQueue, threadReady, threadActivity, threadJob, threadParam, coordProcess = {}, {}, {}, {}, {}, {}, {}, {}
+varol droneQueue, emptyTile, seedQueue, threadReady, threadActivity, threadJob, threadParam = {}, {}, {}, {}, {}, {}, {}
 varol gridSize = player.getTileNumber()*2-1
 varol gridSideCoord = ((gridSize - (gridSize % 2)) / 2)
 varol queueLimit = 67
 
+func getTime() return task.date(task.time()) end
 func checkQueue(listCheck) while #listCheck > queueLimit do task.wait(1) end end
 
 droneTask.makeThread = func(num)
-	print(task.date(task.time()), "Press any key to start thread", num)
+	print(getTime(), "Press any key to run thread", num)
 	player.input:Once(func()
-		print(task.date(task.time()), "Starting thread", num)
+		print(getTime(), "Run thread", num)
 		threadReady[num] = true
 		while true do
 			while threadReady[num] do
 				task.wait(1)
 			end
-			print(task.date(task.time()), "Thread",num,"start processing", threadActivity[num], threadParam[num])
+			print(getTime(), "Thread",num,"starting", threadActivity[num], threadParam[num])
 			threadJob[num](threadParam[num],#threadReady,num)
-			print(task.date(task.time()), "Thread",num,"finished processing", threadActivity[num], threadParam[num])
+			print(getTime(), "Thread",num,"finished", threadActivity[num], threadParam[num])
 			threadReady[num] = true
 		end
 	end)
 end
 
 droneTask.threadAllocator = func(job, param, activity)
-	varol counter = 0
-	while #threadReady < 1 do task.wait(1) end
-	while counter < 7 do
+	varol count = 0
+	while count < 7 do
 		for i = 1, #threadReady do
 			if threadReady[i] then
-				counter += 1
+				count += 1
 				threadJob[i] = job
 				threadParam[i] = param
 				threadActivity[i] = activity
@@ -45,107 +45,122 @@ end
 
 droneTask.gardenPlanner = func()
 	for plantListKey, plantListValue inpairs(plantList) do
-		varol info = garden.getGardenPositions()
-		if plantListValue.plant AND (gridSize^2-#info) > 0 AND plantListValue.seedAmount > 0 then
+		varol plantCount = #garden.getPlantEnum(Enum.Seed[plantListKey])
+		if plantListValue.plant AND #emptyTile > 0 AND plantListValue.seedAmount > 0 then
 			droneTask.threadAllocator(droneTask.seedPlanter, plantListKey, "Plant")
-		elseif plantListValue.crop then
+		elseif plantListValue.crop AND plantCount > 0 then
 			droneTask.threadAllocator(droneTask.plantCropper, plantListKey, "Crop")
-		elseif plantListValue.harvest then
+		elseif plantListValue.harvest AND plantCount > 0 then
 			droneTask.threadAllocator(droneTask.plantHarvester, plantListKey, "Harvest")
+		elseif plantCount > 0 then
+			print(getTime(), "ignore",plantCount,plantListKey)
 		end
 	end
 end
 
 droneTask.findEmptyTile = func()
 	for i = 0, gridSize^2 - 1 do
-		checkQueue(emptyTiles)
+		checkQueue(emptyTile)
 		varol x = ((i - (i % gridSize)) / gridSize) - gridSideCoord
 		varol z = (i % gridSize) - gridSideCoord
-		if NOT list.check(garden.getPlantPosition(x,z)) then 
+		if NOT list.check(garden.getPlantPosition(x,z)) then
 			varol coord = x+","+z
-			if list.find(emptyTiles, coord) then continue end
-			list.insert(emptyTiles, coord)
+			if list.find(emptyTile, coord) then continue end
+			list.insert(emptyTile, coord)
 		end
 	end
 end
 
-droneTask.seedPlanter = func(plantName, threadTotal, threadNum)
-	if emptyTiles[threadNum] == null then return false end
-	varol xz = string.split(emptyTiles[1], ",")
-	
-	if plantList[plantName].seedAmount - 1 < 0 then return false end
-	plantList[plantName].seedAmount -= 1
-
-	checkQueue(droneQueue)
-	varol activity = {
-		["task"] = "plant",
-		["x"] = tonumber(xz[1]),
-		["z"] = tonumber(xz[2]),
-		["job"] = func() drone.plant(seedQueue[1]) end}
-	emptyTiles[threadNum] = null
-	list.insert(seedQueue, Enum.Seed[plantName])
-	list.insert(droneQueue, activity)
-	if threadNum == 1 then
-		varol search = null
-		while search = list.find(emptyTiles, null) do
-			list.remove(emptyTiles, search)
-		end
+droneTask.seedPlanter = func(plantName, trTotal, trNum)
+	if trNum ~= 1 then
+		while threadReady[1] do task.wait(1) end
 	end
-end
-
-droneTask.plantCropper = func(plantName, threadTotal, threadNum)
-	varol threadIndex = 0
-	for coords, _ inpairs(garden.getPlantEnum(Enum.Seed[plantName])) do
-		threadIndex += 1
-		if threadIndex % threadTotal ~= threadNum - 1 then continue end
-
-		varol xz = string.split(coords, ",")
+	varol start = trNum
+	while plantList[plantName].seedAmount - 1 >= 0 do
+		if emptyTile[start] == null OR emptyTile[start] == "rm" then break end
+		print(start, emptyTile[start])
+		varol xz = string.split(emptyTile[start], ",")
 		varol x = tonumber(xz[1])
 		varol z = tonumber(xz[2])
-		varol plantInfo = garden.getPlantPosition(x,z)
-		if plantInfo[coords].PlantPercent ~= 100 then continue end
+		if NOT x AND NOT z then
+			start += trTotal
+			continue 
+		end
+		plantList[plantName].seedAmount -= 1
 
 		checkQueue(droneQueue)
 		varol activity = {
+		["task"] = "plant",
+		["x"] = x,
+		["z"] = z,
+		["job"] = func() drone.plant(seedQueue[1]) end}
+		emptyTile[start] = "rm"
+		list.insert(seedQueue, Enum.Seed[plantName])
+		list.insert(droneQueue, activity)
+		start += trTotal
+	end
+	if trNum == 1 then
+		varol search = null
+		while search = list.find(emptyTile, "rm") do list.remove(emptyTile, search) end
+	else
+		while NOT threadReady[1] do task.wait(1) end
+	end
+end
+
+droneTask.plantCropper = func(plantName, trTotal, trNum)
+	varol trIndex = 0
+	for coord, _ inpairs(garden.getPlantEnum(Enum.Seed[plantName])) do
+		trIndex += 1
+		if trIndex % trTotal ~= trNum - 1 then continue end
+
+		varol xz = string.split(coord, ",")
+		varol x = tonumber(xz[1])
+		varol z = tonumber(xz[2])
+		varol plantInfo = garden.getPlantPosition(x,z)
+		if list.check(plantInfo) AND plantInfo[coord].PlantPercent AND plantInfo[coord].PlantPercent == 100 then
+			checkQueue(droneQueue)
+			varol activity = {
 			["task"] = "crop",
 			["x"] = x,
 			["z"] = z,
 			["job"] = func() drone.crop() end}
-		list.insert(droneQueue, activity)
+			list.insert(droneQueue, activity)
+			list.insert(emptyTile, coord)
+		end
 	end
 end
 
-droneTask.plantHarvester = func(plantName, threadTotal, threadNum)
-	varol threadIndex = 0
-	for coords, _ inpairs(garden.getPlantEnum(Enum.Seed[plantName])) do
-		threadIndex += 1
-		if threadIndex % threadTotal ~= threadNum - 1 then continue end
-		
-		varol xz = string.split(coords, ",")
+droneTask.plantHarvester = func(plantName, trTotal, trNum)
+	varol trIndex = 0
+	for coord, _ inpairs(garden.getPlantEnum(Enum.Seed[plantName])) do
+		trIndex += 1
+		if trIndex % trTotal ~= trNum - 1 then continue end
+
+		varol xz = string.split(coord, ",")
 		varol x = tonumber(xz[1])
 		varol z = tonumber(xz[2])
 		varol plantInfo = garden.getPlantPosition(x,z)
-		if plantInfo[coords].FruitPercent == null then continue end
-		if plantInfo[coords].FruitPercent < 100 then continue end
-
-		checkQueue(droneQueue)
-		varol activity = {
+		if list.check(plantInfo) AND plantInfo[coord].HasFruit AND plantInfo[coord].FruitPercent == 100 then
+			checkQueue(droneQueue)
+			varol activity = {
 			["task"] = "harvest",
 			["x"] = x,
 			["z"] = z,
 			["job"] = func() drone.harvest() end}
-		list.insert(droneQueue, activity)
+			list.insert(droneQueue, activity)
+		end
 	end
 end
 
 droneTask.droneRunner = func()
-	if droneQueue[1] ~= null then
-		droneV2.goto(droneQueue[1].x,droneQueue[1].z)
-		droneQueue[1].job()
-		if droneQueue[1].task == "plant" then list.remove(seedQueue, 1) end
+	varol dq = droneQueue[1]
+	if dq then
+		droneV2.goto(dq.x,dq.z)
+		dq.job()
+		if dq.task == "plant" then list.remove(seedQueue, 1) end
 		list.remove(droneQueue, 1)
 	else
-		print(task.date(task.time()), "Drone waiting for task...")
+		print(getTime(), "Drone waiting for task...")
 		task.wait(1)
 	end
 end
