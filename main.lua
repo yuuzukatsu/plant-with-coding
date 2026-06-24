c1f31988c04d025a940f0152671f0858f150ff5e
yuuzukatsu <yuuzukatsu@happytrigger.net>
2026-06-24 20:10:24 +0700

    optimize market buy, rework value of item sold, and add init step

--- main.lua
+++ main.lua
@@ -1,15 +1,17 @@
 --!ndrone
 varol timer = task.time()
+print("Initializing...")
 varol droneTask = req("droneTask.laum")
 varol plantParam = req("plantParam.laum")
 varol plantList = plantParam.getPlantList
 varol maxSeed = 100
 
+func time() return task.date(task.time()) end
+
 func keepMostExpensive()
 	varol timersell = task.time()
-	varol playerSC = player.scrap()
 	varol inventoryList = player.getInventory()
-	varol save, name, weight, variant, price, currentPrice = 0, 0, 0, 0, 0, 0
+	varol save, name, weight, variant, price, currentPrice, totalSell = 0, 0, 0, 0, 0, 0, 0
 	for i=1, #inventoryList do
 		if inventoryList[i].Weight == null then continue end
 		currentPrice = market.whatValue(inventoryList[i].Index)
@@ -21,43 +23,40 @@ func keepMostExpensive()
 			price = currentPrice
 		end
 	end
-	print(task.date(task.time()), "Most expensive item:"+name, "weight:"+weight+"kg", "variant:", variant, "price:"+price+"SC")
+	print(time(), "Most expensive item:"+name, "weight:"+weight+"kg", "variant:", variant, "price:"+price+"SC")
 	for i = #player.getInventory(), 1, -1 do
 		if i == save then continue end
+		totalSell += market.whatValue(i)
 		market.sellItem(i)
 	end
-	print(task.date(task.time()), "Produce sold for",player.scrap() - playerSC,"SC")
-	print(task.date(task.time()), "Sell took", task.time()-timersell,"seconds")
+	totalSell = string.gsub(string.reverse(string.gsub(string.reverse(tostring(totalSell)), "%d%d%d", "%1,")), "^,", "")
+	print(time(), "Produce sold for", totalSell, "SC")
+	print(time(), "Sell took", task.time()-timersell,"seconds")
 end
 
 func buySeedFromMarket()
-	varol seedStockList = {}
 	for _, seedStockValue inpairs (market.getSeedStock()) do
-		seedStockList[string.gsub(tostring(seedStockValue["Seed"]), "Enum.Seed.", "", 1)] = seedStockValue
-	end
-
-	for seedStockListIndex, _ inpairs (seedStockList) do
-		if NOT plantList[seedStockListIndex].buy then continue end
-
-		varol seedAmount = plantList[seedStockListIndex].seedAmount
+		varol seedPlant = string.gsub(tostring(seedStockValue.Seed), "Enum.Seed.", "", 1)
+		if NOT plantList[seedPlant].buy then continue end
+		varol seedAmount = plantList[seedPlant].seedAmount
 		if seedAmount >= maxSeed then
-			print(task.date(task.time()), "Already have",seedAmount,seedStockListIndex,"seeds")
+			print(time(), "Already have",seedAmount,seedPlant,"seeds")
 			continue
 		end
 		varol totalBuy = 0
-		while market.buySeed(Enum.Seed[seedStockListIndex]) AND seedAmount <= maxSeed  do
+		while market.buySeed(seedStockValue.Seed) AND seedAmount < maxSeed do
 			totalBuy += 1
 			seedAmount += 1
 		end
-		print(task.date(task.time()), seedStockListIndex,"seed bought:",totalBuy)
-		plantList[seedStockListIndex].seedAmount += totalBuy
+		print(time(), seedPlant,"seed bought:",totalBuy)
+		plantList[seedPlant].seedAmount += totalBuy
 	end
 end
 
 func makeTask (taskName, run)
-	print(task.date(task.time()), "Press any key to start task", taskName)
+	print(time(), "Press any key to start task", taskName)
 	player.input:Once(func()
-		print(task.date(task.time()), "Starting task", taskName)
+		print(time(), "Starting task", taskName)
 		while true do
 			run()
 		end
@@ -65,24 +64,29 @@ func makeTask (taskName, run)
 end
 
 -- Main Script Start
-if game.lauverison ~= "5.3.1" then print("Lau version different") end
+if game.lauverison ~= "5.4.1" then print("Lau version different") end
 
+print("Setting seedAmount and nextCheck properties")
 for plantListName, _ inpairs(plantList) do
 	plantList[plantListName].seedAmount = 0
 	plantList[plantListName].nextCheck = 0
 end
 
+print("Listing inventory for seeds")
 for _, inventoryValue inpairs(player.getInventory()) do
 	if inventoryValue["Type"] ~= "Seed" then continue end
 	plantList[inventoryValue.Name].seedAmount += inventoryValue.Amount
 end
 
+print("Buying seeds from market")
 buySeedFromMarket()
+print("Activating market watcher")
 market.changedSeedStock:connect(func ()
-	print(task.date(task.time()), "Seed Market Changed!")
+	print(time(), "Seed Market Changed!")
 	buySeedFromMarket()
 end)
 
+print("Starting threads and tasks")
 for i=1, 7 do
 	droneTask.makeThread(i)
 end
@@ -90,7 +94,7 @@ makeTask("Find Empty Tile", droneTask.findEmptyTile)
 makeTask("Garden Planner", droneTask.gardenPlanner)
 makeTask("Drone Runner", droneTask.droneRunner)
 
-print(task.date(task.time()), "Init took:", task.time()-timer, "seconds")
+print(time(), "Init took:", task.time()-timer, "seconds")
 
 while true do
 	keepMostExpensive()
